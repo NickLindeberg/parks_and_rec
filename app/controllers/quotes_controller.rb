18b5047a77eb93ae82fe4fe0f831bf289be00b7e
Nick Lindeberg <lindebergnick@gmail.com>
2022-03-26 17:26:57 -0700

    add quote to partial, clean up spec, refresh button

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,14 @@
 class QuotesController < ApplicationController
-
   def index
     @quote = service.quote.first
+    @ip = user_ip
   end
 
   private
+
+  def user_ip
+    request.remote_ip
+  end
 
   def service
     SwansonService.new('quote')
--- spec/features/quotes_feature_spec.rb
+++ spec/features/quotes_feature_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 describe 'Quotes' do
+  let(:option)   { 'quote' }
+  let(:service)  { SwansonService.new(option) }
+  let(:response) { service.quote }
+  let(:quote)    { 'Fish, for sport only, not for meat. Fish meat is practically a vegetable.' }
+
   it 'shows quote on index' do
     VCR.use_cassette('ron_thoughts_casette') do
       visit quotes_path
 
-      expect(page).to have_content('Something Funny Goes Here')
+      expect(page).to have_content(quote)
     end
   end
 end
